f2ac1fc74e11bed8e078bff6fc368416d8917b67
dalimvc <dalibor_mirkovic@live.com>
2024-02-07 18:27:17 +0100

    program works, needs small fixes

--- Lottoprogram/MainPage.xaml.cs
+++ Lottoprogram/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -18,9 +19,6 @@ using Windows.UI.Xaml.Navigation;
 
 namespace Lottoprogram
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
@@ -28,45 +26,180 @@ namespace Lottoprogram
             this.InitializeComponent();
         }
 
-        int value1;
-        int value2;
-        int value3;
-        int value4;
-        int value5;
-        int value6;
-        int value7;
+        string errorMessage;
+        int rounds;
+        int fives = 0;
+        int sixes = 0;
+        int sevens = 0;
+
+        Random randomNumber = new Random();
+
+        List<int> enteredLottoNumbers = new List<int>();
+
+        List<List<int>> generatedLottoSeries = new List<List<int>>();   
 
         public void GetNumbers()
         {
-                value1 = int.Parse(number1.Text);
-                value2 = int.Parse(number2.Text);
-                value3 = int.Parse(number3.Text);
-                value4 = int.Parse(number4.Text);
-                value5 = int.Parse(number5.Text);
-                value6 = int.Parse(number6.Text);
-                value7 = int.Parse(number7.Text);     
+            errorMessage= string.Empty;
+            enteredLottoNumbers.Clear();
 
+            try
+            {
+                int.TryParse(number1.Text, out int value1);
+                int.TryParse(number2.Text, out int value2);
+                int.TryParse(number3.Text, out int value3);
+                int.TryParse(number4.Text, out int value4);
+                int.TryParse(number5.Text, out int value5);
+                int.TryParse(number6.Text, out int value6);
+                int.TryParse(number7.Text, out int value7);
+
+                enteredLottoNumbers.Add(value1);
+                enteredLottoNumbers.Add(value2);
+                enteredLottoNumbers.Add(value3);
+                enteredLottoNumbers.Add(value4);
+                enteredLottoNumbers.Add(value5);
+                enteredLottoNumbers.Add(value6);
+                enteredLottoNumbers.Add(value7);
+            }
+            catch{
+                errorMessage = "All fields are required and you can enter only numbers between 1-35!";
+            }
+            ErrorMessageTextBlock.Text = errorMessage;
+        }
+
+        public void GetNrOfRounds()
+        {   
+            try
+            {
+                rounds = int.Parse(nrDraws.Text);
+            }
+            catch
+            {
+                errorMessage = "Number of rounds must be a positive number!";
+            }
+            ErrorMessageTextBlock.Text = errorMessage;
         }
 
         public void CheckNumberValidity()
         {
-
+            for (int i = 0; i< enteredLottoNumbers.Count -1; i++)
+            {
+                if (enteredLottoNumbers[i] < 1 || enteredLottoNumbers[i] > 35)
+                {
+                    errorMessage = "Numbers must be between 1 and 35! Wrong entry.";
+                }
+                else if (enteredLottoNumbers[i] == enteredLottoNumbers[i + 1])
+                {
+                    errorMessage = "You can't enter same number more than once! Wrong entry.";
+                } 
+                else if (rounds < 0)
+                {
+                    errorMessage = "Number of rounds must be a positive number! Wrong entry.";
+                }     
+            }
+            ErrorMessageTextBlock.Text = errorMessage;
         }
+
+        public List<int> generateLottoNumbers()
+        {
+            List<int> generatedLottoNumbers = new List<int>();
+            generatedLottoNumbers.Add(randomNumber.Next(1, 35));
+            int counter = 0;
+            while (counter < 6)
+            {
+                int lottoNumber = randomNumber.Next(1, 35);
+                if (!generatedLottoNumbers.Contains(lottoNumber))
+                {
+                    generatedLottoNumbers.Add(lottoNumber);
+                    counter++;
+                }
+            }
+            return generatedLottoNumbers;
+        }
+
+
+
+        public List<List<int>> generateLottoSeriers()
+        {
+            generatedLottoSeries.Clear();
+            int counter = 0;
+            while (rounds > counter)
+            {
+                generatedLottoSeries.Add(generateLottoNumbers());
+                counter++;
+            }
+
+
+
+
+
+
+
+
+            Debug.WriteLine(generatedLottoSeries.Count);
+            return generatedLottoSeries;
+        }
+
+        public int CountOccurrences()
+        {
+            sevens= 0;
+            sixes= 0;
+            fives= 0;
+            int counterMatches = 0;
+            foreach (List<int> lottoCombination in generatedLottoSeries)
+            {
+                counterMatches = 0;
+                foreach (int number in enteredLottoNumbers)
+                {
+                    if (lottoCombination.Contains(number))
+                    {
+                        counterMatches++;
+                        if (counterMatches == 7)
+                        {
+                            sevens++;
+                        } else if(counterMatches == 6)
+                        {
+                            sixes++;
+                        } else if(counterMatches == 5)
+                        {
+                            fives++;
+                        }
+                    }
+                }
+            }
+            return counterMatches;
+        }
+
+        public void updateTextresult()
+        {
+            fiveMatches.Text = fives.ToString();
+            sixMatches.Text = sixes.ToString();
+            SevenMatches.Text = sevens.ToString();
+        }
+
+
+
 
         private void RunLotto(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                GetNumbers();
-            }
-            catch
-            {
-                Debug.WriteLine(e);
-            }
-            
 
-            Debug.WriteLine("lallaa");
-            Debug.WriteLine(value1);
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            updateTextresult();
+            GetNumbers();
+            GetNrOfRounds();
+            CheckNumberValidity();
+            generateLottoSeriers();
+            CountOccurrences();
+            updateTextresult();
+
+
+            stopwatch.Stop();
+            Debug.WriteLine($"Time taken to generate {rounds} rounds: {stopwatch.Elapsed.TotalSeconds} seconds");
+            Debug.WriteLine("Fives: " + fives);
+            Debug.WriteLine("Sixes: " + sixes);
+            Debug.WriteLine("Sevens: " + sevens);
         }
 
 
